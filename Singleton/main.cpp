090e5d05df07150c22dba318abba18cd28c560d1
harkhuang <shiyanhk@gmail.com>
2020-10-23 00:32:14 +0800

    update

--- Singleton/main.cpp
+++ Singleton/main.cpp
@@ -4,11 +4,16 @@
 
 class President {
 public:
+
+
+   // 在c11 中static 表示线程安全
     static President& GetInstance() {
         static President instance;
         return instance;
     }
 
+
+// 删除了拷贝构造 使得对象更加的安全
     President(const President&) = delete;
     President& operator=(const President&) = delete;
 
