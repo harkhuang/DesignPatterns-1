39e3c0b8711254a7bfe62eb8570a86473338603f
huangkui <huangkui@ysstech.com>
2020-10-23 18:12:18 +0800

    更新

--- ChainOfResponsibility/main.cpp
+++ ChainOfResponsibility/main.cpp
@@ -7,12 +7,19 @@ public:
     // 初始化
     Account(float balance) : balance_(balance) {}
     virtual std::string GetClassName() { return "Account"; }
+
+
+
+    // 设置后继类指针的指向  
+    // 结构体就是一个指针节点
+    
     void SetNext(Account* const account) { successor_ = account; }
     bool CanPay(float amount) { return balance_ >= amount; }
 
 
 
     // 自己调用自己  巧妙的使用递归调用   传递责任链条
+    // 提供递归方式对指针进行逐个访问
     void Pay(float amountToPay) {
 
         // 成功支付
@@ -54,11 +61,13 @@ public:
     std::string GetClassName() override { return "Bitcoin"; }
 };
 
-//class ZhiFuBao : public Account {
-//public:
-//    ZhiFuBao(float balance) : Account(balance) {}
-//    std::string GetClassName() override { return "ZhiFuBao"; }
-//};
+
+// 添加后继指针节点
+class ZhiFuBao : public Account {
+public:
+	ZhiFuBao(float balance) : Account(balance) {}
+	std::string GetClassName() override { return "ZhiFuBao"; }
+};
 
 
 
@@ -79,13 +88,24 @@ int main()
 
     paypal.SetNext(&bitcoin);
 
-  //  ZhiFuBao zhifubao(400);
+    ZhiFuBao zhifubao(400);
 
-//    zhifubao.SetNext(&paypal);
+    bitcoin.SetNext(&zhifubao);
 
     // 仅支付中做出选择
     // 选择是要过三个账户余额的 找到最合适的
+    
+    // 尝试从银行开始支付99元
+    std::cout << "try to pay use bank first.and then...\n";
     bank.Pay(99);
     bank.Pay(199);
     bank.Pay(299);
+    bank.Pay(399);
+
+
+	std::cout << "try to pay use papal first.and then...\n";
+    paypal.Pay(299);
+
+
+
 }
--- Flyweight/main.cpp
+++ Flyweight/main.cpp
@@ -6,19 +6,35 @@
 // Types of tea here will be flyweights.
 class KarakTea {};
 
+// 享元模式最大的特点就是
+
+
+
 class TeaMaker {
 public:
     KarakTea* Make(const std::string& preference) {
+
+        // 查询该记录是否存在  不存在则插入 并且返回该对象
         if (availableTea_.find(preference) == availableTea_.end())
+            // 如果不存在在这里构建
             availableTea_.insert({preference, new KarakTea()});
 
         return availableTea_.at(preference);
     }
 
 private:
+
+    // 记录享元模式内容
+    // 记录能够做的奶茶的种类?
+
+    // 记录了该之怎对象
     std::unordered_map<std::string, KarakTea*> availableTea_;
 };
 
+
+
+
+// 没有继承只有关联
 class TeaShop {
 public:
     TeaShop(TeaMaker& teaMaker): teaMaker_(teaMaker) {}
@@ -31,18 +47,31 @@ public:
     }
 
 private:
+
+    // int 表示座位号码    karak表示茶的种类
     std::unordered_map<int, KarakTea*> orders_;
     TeaMaker& teaMaker_;
 };
 
 int main()
 {
+
+    // 奶茶师
     TeaMaker teaMaker;
+
+    //  奶茶店
     TeaShop shop(teaMaker);
 
+
+    // 奶茶店创建奶茶
+    // 奶茶店享受奶茶师的技能和工具  
+    // 通过奶茶店制造各种奶茶
     shop.TakeOrder("less sugar", 1);
-    shop.TakeOrder("more milk", 2);
+	shop.TakeOrder("more milk", 2);
+	shop.TakeOrder("less milk",3);
     shop.TakeOrder("without sugar", 5);
 
     shop.Serve();
+
+    getchar();
 }
